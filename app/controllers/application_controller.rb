9efe55f8516eb7d68ea8ce1a5ccb173cf7559212
Sheena Sang <sheenasany@gmail.com>
2022-09-11 23:45:30 -0400

    commit tree 039b73d0ab5b64e1d7a4320643b26a2735a8ba43

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,28 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  get '/' do
-    { message: "Hello world" }.to_json
+    get '/games' do
+      # get all the games from the database
+      games = Game.all.order(:title).limit(10)
+      # return a JSON response with an array of all the game data
+      games.to_json
+    end
+
+    # use the :id syntax to create a dynamic route
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    game.to_json(include: { reviews: { include: :user } })
+
+     # include associated reviews in the JSON response
+     game.to_json(include: { reviews: { include: :user } })
+
+     game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
-
-end
+  
+  end
